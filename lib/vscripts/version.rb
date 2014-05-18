5f11c0616e806af127a187740f845e0a23939211
Vlad <vg@vladgh.com>
2014-05-18 07:04:05 -0700

    Copyright year

--- lib/vscripts/version.rb
+++ lib/vscripts/version.rb
@@ -2,5 +2,5 @@
 module VScripts
   VERSION = File.read(File.expand_path('../../../VERSION', __FILE__)).strip
   VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH = VERSION.split('.')
-  VERSION_C = "VScripts #{VERSION} (c) 2014 Vlad Ghinea"
+  VERSION_C = "VScripts #{VERSION} (c) #{Time.new.year} Vlad Ghinea"
 end
