8a52a9b800d06374c9ee5ee4872b055af6f9a3be
Vlad Ghinea <vg@vladgh.com>
2014-06-03 18:10:18 -0700

    Add local code coverage files for development

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
+# Track your code coverage
 require 'coveralls'
 Coveralls.wear!
+
+# Development code coverage
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  Coveralls::SimpleCov::Formatter,
+  SimpleCov::Formatter::HTMLFormatter
+]
 
 class DummyClass
 end
