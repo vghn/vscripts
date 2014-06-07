3c4bd753de4f324aac29ed1d94fdd1ed3a19b6bc
Vlad <vlad@ghn.me>
2014-06-07 13:55:37 -0500

    Better RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,20 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter
 ]
 
-class DummyClass
-end
+# RSpec Configuration
+RSpec.configure {|config|
+  # Abort the run on first failure.
+  config.fail_fast = true
+
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+
+  # Clean up
+  config.after(:suite) { `ls /tmp/vscripts-test-* &> /dev/null && rm -r /tmp/vscripts-test-*` }
+}
